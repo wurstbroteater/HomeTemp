12697373750f323d5b07690b4b449d004ed44f36
wurstbroteater <fleri94@web.de>
2023-09-01 19:44:31 +0200

    updated formatting, removed requirement, update gitignore

--- humidity.py
+++ humidity.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText 
+from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
@@ -21,14 +21,14 @@ config.read('hometemp.ini')
 log = logging.getLogger('hometemp')
 
 
-
 # ------------------------------- Raspberry Pi Temps ----------------------------------------------
 
 def get_temperature():
     cpu = CPUTemperature()
     return float(cpu.temperature)
 
-def get_sensor_data() :
+
+def get_sensor_data():
     """
     Returns temperature and humidity data measures by AM2302 Sensor and the measure timestamp or raise exception 
     if could not be retrieved
@@ -44,22 +44,24 @@ def get_sensor_data() :
     else:
         raise Exception("Failed to retrieve data from humidity sensor")
 
+
 # ------------------------------- POSTGRES ----------------------------------------------
 
 def _create_table(connection, table_name="sensor_data"):
-    metadata = MetaData() 
-    table_schema = Table(table_name, metadata, 
-        Column('id', Integer, primary_key=True, autoincrement=True),
-        Column('timestamp', TIMESTAMP(timezone=True), nullable=False),
-        Column('humidity', DECIMAL, nullable=False),
-        Column('room_temp', DECIMAL, nullable=False),
-        Column('cpu_temp', DECIMAL, nullable=False))
+    metadata = MetaData()
+    table_schema = Table(table_name, metadata,
+                         Column('id', Integer, primary_key=True, autoincrement=True),
+                         Column('timestamp', TIMESTAMP(timezone=True), nullable=False),
+                         Column('humidity', DECIMAL, nullable=False),
+                         Column('room_temp', DECIMAL, nullable=False),
+                         Column('cpu_temp', DECIMAL, nullable=False))
     try:
         metadata.create_all(connection)
         log.info(f"Table '{table_name}' created successfully.")
 
     except exc.SQLAlchemyError as e:
         log.error("Problem with database " + str(e))
+
 
 def _remove_table(connection, table_name="sensor_data"):
     try:
@@ -86,12 +88,13 @@ def _get_table_size(connection, table_name="sensor_data"):
 def _clear_table(connection, table_name="sensor_data"):
     try:
         table = Table(table_name, MetaData(), autoload_with=connection)
-        with connection.begin() as con :
+        with connection.begin() as con:
             con.execute(table.delete())
             log.info(f"Table '{table_name}' cleared successfully.")
 
     except exc.SQLAlchemyError as e:
         log.error("Problem with database " + str(e))
+
 
 def _check_table_existence(connection, table_name="sensor_data"):
     try:
@@ -100,7 +103,7 @@ def _check_table_existence(connection, table_name="sensor_data"):
     except exc.SQLAlchemyError as e:
         log.error("Problem with database " + str(e))
         return False
-    
+
 
 def _init_db():
     auth = config["db"]
@@ -125,19 +128,18 @@ def init_db_connection(table_name="sensor_data"):
         log.error("Problem with database " + str(e))
 
 
-
 def insert_measurements_into_db(connection, timestamp, humidity, room_temp, cpu_temp, table_name="sensor_data"):
     try:
         table = Table(table_name, MetaData(), autoload_with=connection, extend_existing=True)
         with connection.begin() as con:
-             data_to_insert = {
-                 'timestamp': timestamp,
-                 'humidity': humidity,
-                 'room_temp': room_temp,
-                 'cpu_temp': cpu_temp
-             }
-             insert_statement = insert(table).values(**data_to_insert)   
-             con.execute(insert_statement)
+            data_to_insert = {
+                'timestamp': timestamp,
+                'humidity': humidity,
+                'room_temp': room_temp,
+                'cpu_temp': cpu_temp
+            }
+            insert_statement = insert(table).values(**data_to_insert)
+            con.execute(insert_statement)
 
     except exc.SQLAlchemyError as e:
         log.error("Problem with database " + str(e))
@@ -151,7 +153,8 @@ def read_data_into_dataframe(connection, table_name="sensor_data"):
     except exc.SQLAlchemyError as e:
         log.error("Problem with database " + str(e))
         return None
-    
+
+
 # ------------------------------- Data Distribution ----------------------------------------------
 
 def send_visualization_email(df):
@@ -163,7 +166,9 @@ def send_visualization_email(df):
     log.info(f"Sending Measurement Data Visualization to {from_email}")
 
     subject = f"Measurement Data Report {file_name}"
-    message = str(df[["humidity", "room_temp", "cpu_temp"]].corr()) + "\n\n"+ str(df[["humidity", "room_temp", "cpu_temp"]].describe()).format("utf8") + "\n\n" + str(df[["timestamp", "humidity", "room_temp", "cpu_temp"]].tail(6))
+    message = str(df[["humidity", "room_temp", "cpu_temp"]].corr()) + "\n\n" + str(
+        df[["humidity", "room_temp", "cpu_temp"]].describe()).format("utf8") + "\n\n" + str(
+        df[["timestamp", "humidity", "room_temp", "cpu_temp"]].tail(6))
 
     pdf_file_path = f"/home/eric/HomeTemp/plots/{file_name}.pdf"
     attachment = open(pdf_file_path, "rb")
@@ -193,12 +198,13 @@ def send_visualization_email(df):
     finally:
         attachment.close()
     log.info("Done")
-        
+
+
 # ------------------------------- Visualization ----------------------------------------------
 
 def draw_plots(df, show_heatmap=True):
     subplots = 3 if show_heatmap else 2
-    sns.set_theme(style="darkgrid")#sns.set(style="whitegrid")
+    sns.set_theme(style="darkgrid")  # sns.set(style="whitegrid")
     fig = plt.figure(figsize=(25, 12))
     gs = fig.add_gridspec(2, 2, height_ratios=[2, 2])  # 2 rows, 1 column
 
@@ -210,8 +216,8 @@ def draw_plots(df, show_heatmap=True):
     plt.ylabel("Temp (°C)")
     plt.legend()
     plt.xticks(rotation=45)
-    #plt.tight_layout()
-    #plt.show()
+    # plt.tight_layout()
+    # plt.show()
 
     # Humidity Measurement
     plt.subplot(gs[1])
@@ -228,7 +234,7 @@ def draw_plots(df, show_heatmap=True):
 
     # Temperature Measurements last 24 h
     plt.subplot(gs[2])
-    sns.lineplot(label="Home", x="timestamp", y="room_temp",marker='o',markersize=6, data=df_last_24h)
+    sns.lineplot(label="Home", x="timestamp", y="room_temp", marker='o', markersize=6, data=df_last_24h)
     plt.title("Temperature Last 24 Hours")
     plt.xlabel("Time")
     plt.ylabel("Temp (°C)")
@@ -253,15 +259,16 @@ def draw_plots(df, show_heatmap=True):
     plt.close()
     sns.reset_defaults()
 
+
 # ------------------------------- Main  ----------------------------------------------
 
 def create_and_backup_visualization():
     log.info("Creating Measurement Data Visualization")
-    con =  init_db_connection()
+    con = init_db_connection()
     df = read_data_into_dataframe(con)
     df = df.sort_values(by="timestamp")
-    df['timestamp'] = df['timestamp'].map(lambda x : datetime.strptime(str(x).strip(), '%Y-%m-%d %H:%M:%S'))
-    draw_plots(df,show_heatmap=False)
+    df['timestamp'] = df['timestamp'].map(lambda x: datetime.strptime(str(x).strip(), '%Y-%m-%d %H:%M:%S'))
+    draw_plots(df, show_heatmap=False)
     log.info("Done")
     send_visualization_email(df)
 
@@ -269,12 +276,14 @@ def create_and_backup_visualization():
 def collect_and_save_to_db():
     log.info("Start Measurement Data Collection")
     con = init_db_connection()
-    #log.debug(_get_table_size(con))
-    #_clear_table(con)
+    # log.debug(_get_table_size(con))
+    # _clear_table(con)
     cpu_temp = get_temperature()
     room_temp, humidity, timestamp = get_sensor_data()
-    log.info("[Measurement {0}] CPU={1:f}*C, Room={2:f}*C, Humidity={3:f}%".format(timestamp, cpu_temp, room_temp, humidity))
-    insert_measurements_into_db(connection=con,timestamp=timestamp, humidity=humidity, room_temp=room_temp, cpu_temp=cpu_temp)
+    log.info(
+        "[Measurement {0}] CPU={1:f}*C, Room={2:f}*C, Humidity={3:f}%".format(timestamp, cpu_temp, room_temp, humidity))
+    insert_measurements_into_db(connection=con, timestamp=timestamp, humidity=humidity, room_temp=room_temp,
+                                cpu_temp=cpu_temp)
     log.info("Done")
 
 
@@ -289,7 +298,7 @@ def main():
     create_and_backup_visualization()
     while True:
         schedule.run_pending()
-        time.sleep(1) 
+        time.sleep(1)
 
 
 if __name__ == "__main__":
