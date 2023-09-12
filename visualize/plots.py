957abd900b67e999d7df2affe164bd47c9ef3220
eric <fleri94@web.de>
2023-09-12 17:37:28 +0200

    fix 24h plots containing larger time scales

--- visualize/plots.py
+++ visualize/plots.py
@@ -39,14 +39,16 @@ def draw_plots(df, dwd_df=None, google_df=None, with_save=True):
     sns.despine(left=True, bottom=True)
 
     df_last_24h = df[df["timestamp"] >= datetime.now() - timedelta(hours=25)]
+    google_df_last_24h = google_df[google_df["timestamp"] >= datetime.now() - timedelta(hours=25)]
+    dwd_df_last_24h = dwd_df[dwd_df["timestamp"] >= datetime.now() - timedelta(hours=25)]
 
     # Temperature Measurements last 24 h
     plt.subplot(gs[2])
     sns.lineplot(label="Home", x="timestamp", y="room_temp", marker='o', markersize=6, data=df_last_24h)
     if dwd_df is not None:
-        sns.lineplot(label="DWD Forecast", x="timestamp", y="temp", marker='o', markersize=6, data=dwd_df)
+        sns.lineplot(label="DWD Forecast", x="timestamp", y="temp", marker='o', markersize=6, data=dwd_df_last_24h)
     if google_df is not None:
-        sns.lineplot(label="Google Forecast", x="timestamp", y="temp", marker='o', markersize=6, data=google_df)
+        sns.lineplot(label="Google Forecast", x="timestamp", y="temp", marker='o', markersize=6, data=google_df_last_24h)
     plt.title("Temperature Last 24 Hours")
     plt.xlabel("Time")
     plt.ylabel("Temp (°C)")
@@ -57,7 +59,7 @@ def draw_plots(df, dwd_df=None, google_df=None, with_save=True):
     plt.subplot(gs[3])
     sns.lineplot(label="Home", x="timestamp", y="humidity", marker='o', markersize=6, color='purple', data=df_last_24h)
     if google_df is not None:
-        sns.lineplot(label="Google Forecast", x="timestamp", y="humidity", marker='o', markersize=6, data=google_df)
+        sns.lineplot(label="Google Forecast", x="timestamp", y="humidity", marker='o', markersize=6, data=google_df_last_24h)
     plt.title("Humidity Last 24 Hours")
     plt.xlabel("Time")
     plt.ylabel("Humidity (%)")
