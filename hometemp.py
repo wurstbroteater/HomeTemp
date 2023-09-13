9c7ed047973079f58746cbb4ac0710362bba3ebd
eric <fleri94@web.de>
2023-09-13 16:07:22 +0200

    refactored data distribution, this closes #5

--- hometemp.py
+++ hometemp.py
@@ -1,10 +1,7 @@
-import Adafruit_DHT, configparser, logging, re, schedule, smtplib, time, threading
+import Adafruit_DHT, configparser, logging, re, schedule, time, threading
 from datetime import datetime, timedelta
-from gpiozero import CPUTemperature
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
+from gpiozero import CPUTemperature#
+from distribute.email import EmailDistributor
 from persist.database import DwDDataHandler, GoogleDataHandler, SensorDataHandler 
 from visualize.plots import draw_plots
 
@@ -43,58 +40,6 @@ def get_sensor_data():
         raise Exception("Failed to retrieve data from humidity sensor")
 
 
-# ------------------------------- Data Distribution ----------------------------------------------
-
-def send_visualization_email(df, google_df, dwd_df):
-    file_name = datetime.now().strftime("%d-%m-%Y")
-    auth = config["distribution"]
-
-    from_email = auth["from_email"]
-    to_email = auth["to_email"]
-    log.info(f"Sending Measurement Data Visualization to {from_email}")
-
-    subject = f"HomeTemp v{config['hometemp']['version']} Data Report {file_name}"
-    message = "------------- Sensor Data -------------\n"
-    message += str(df[["humidity", "room_temp", "cpu_temp"]].corr()) + "\n\n"
-    message += str(df[["humidity", "room_temp", "cpu_temp"]].describe()).format("utf8") + "\n\n"
-    message += str(df[["timestamp", "humidity", "room_temp", "cpu_temp"]].tail(6))
-    message += "\n\n------------- Google Data -------------\n"
-    message += str(google_df.drop(['id', 'timestamp'],axis=1).describe()).format("utf8") + "\n\n"
-    message += str(google_df.tail(6))
-    message += "\n\n------------- DWD Data -------------\n"
-    message += str(dwd_df.drop(['id', 'timestamp'],axis=1).describe()).format("utf8") + "\n\n"
-    message += str(dwd_df.tail(6))
-
-    pdf_file_path = f"/home/eric/HomeTemp/plots/{file_name}.pdf"
-    attachment = open(pdf_file_path, "rb")
-
-    msg = MIMEMultipart()
-    msg["From"] = from_email
-    msg["To"] = to_email
-    msg["Subject"] = subject
-
-    msg.attach(MIMEText(message, "plain"))
-
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header("Content-Disposition", f"attachment; filename= {file_name}.pdf")
-    msg.attach(part)
-
-    try:
-        server = smtplib.SMTP(auth["smtp_server"], auth["smtp_port"])
-        server.starttls()
-        server.login(auth["smtp_user"], auth["smtp_pw"])
-        server.sendmail(from_email, to_email, msg.as_string())
-        server.quit()
-        log.info("Email sent successfully.")
-    except Exception as e:
-        log.error(f"Error sending email: {str(e)}")
-    finally:
-        attachment.close()
-    log.info("Done")
-
-
 # ------------------------------- Main  ----------------------------------------------
 
 def create_and_backup_visualization():
@@ -120,7 +65,7 @@ def create_and_backup_visualization():
     dwd_df = dwd_df.sort_values(by="timestamp")
     draw_plots(df, google_df=google_df, dwd_df=dwd_df)
     log.info("Done")
-    send_visualization_email(df, google_df=google_df, dwd_df=dwd_df)
+    EmailDistributor.send_visualization_email(df, google_df=google_df, dwd_df=dwd_df)
 
 
 def run_threaded(job_func):
@@ -151,7 +96,7 @@ def main():
     # run_threaded assumes that we never have overlapping usage of this method or its components
     schedule.every().day.at("06:00").do(run_threaded, create_and_backup_visualization)
 
-    #collect_and_save_to_db()
+    collect_and_save_to_db()
     #run_threaded(create_and_backup_visualization)
     log.info("finished initialization")
     while True:
