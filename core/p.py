2cf818a8fb409187ac03a80039d223a4ab21056c
wurstbroteater <fleri94@web.de>
2024-09-14 16:48:48 +0200

    auto format code

--- core/p.py
+++ core/p.py
@@ -12,77 +12,79 @@ import logging
 log = logging.getLogger(__name__)
 
 custom_theme = {
-    #'context': 'notebook',   # Can be 'paper', 'notebook', 'talk', 'poster'
-    'style': 'darkgrid',    # Can be 'darkgrid', 'whitegrid', 'dark', 'white', 'ticks'
-    'palette': 'deep'        # Can be 'deep', 'muted', 'bright', 'pastel', etc.
+    # 'context': 'notebook',   # Can be 'paper', 'notebook', 'talk', 'poster'
+    'style': 'darkgrid',  # Can be 'darkgrid', 'whitegrid', 'dark', 'white', 'ticks'
+    'palette': 'deep'  # Can be 'deep', 'muted', 'bright', 'pastel', etc.
 }
 
-def last_24h_df(_df:pd.DataFrame) -> pd.DataFrame:
+
+def last_24h_df(_df: pd.DataFrame) -> pd.DataFrame:
     last_24h = datetime.now() - timedelta(hours=24)
     return _df[_df['timestamp'] >= last_24h]
 
 
-
 def draw_plots(df, dwd_df=None, google_df=None, wettercom_df=None, ulmde_df=None, with_save=True, save_path=None):
     df_temp_inner_plt_params = [
-                inner_plots_params(dwd_df, "DWD Forecast", "timestamp", "temp"),
-                inner_plots_params(google_df, "Google Forecast", "timestamp", "temp"),
-                inner_plots_params(wettercom_df, "Wetter.com Forecast", "timestamp", "temp_stat"),
-                inner_plots_params(wettercom_df, "Wetter.com Live", "timestamp", "temp_dyn"),
-                inner_plots_params(ulmde_df, "Ulm Forecast", "timestamp", "temp")
-                ]
+        inner_plots_params(dwd_df, "DWD Forecast", "timestamp", "temp"),
+        inner_plots_params(google_df, "Google Forecast", "timestamp", "temp"),
+        inner_plots_params(wettercom_df, "Wetter.com Forecast", "timestamp", "temp_stat"),
+        inner_plots_params(wettercom_df, "Wetter.com Live", "timestamp", "temp_dyn"),
+        inner_plots_params(ulmde_df, "Ulm Forecast", "timestamp", "temp")
+    ]
     df_temp_24_inner_plt_params = [
-                inner_24_plots_params(dwd_df, "DWD Forecast", "timestamp", "temp"),
-                inner_24_plots_params(google_df, "Google Forecast", "timestamp", "temp"),
-                inner_24_plots_params(wettercom_df, "Wetter.com Forecast", "timestamp", "temp_stat"),
-                inner_24_plots_params(wettercom_df, "Wetter.com Live", "timestamp", "temp_dyn", marker="s"),
-                inner_24_plots_params(ulmde_df, "Ulm Forecast", "timestamp", "temp")
-                ]
+        inner_24_plots_params(dwd_df, "DWD Forecast", "timestamp", "temp"),
+        inner_24_plots_params(google_df, "Google Forecast", "timestamp", "temp"),
+        inner_24_plots_params(wettercom_df, "Wetter.com Forecast", "timestamp", "temp_stat"),
+        inner_24_plots_params(wettercom_df, "Wetter.com Live", "timestamp", "temp_dyn", marker="s"),
+        inner_24_plots_params(ulmde_df, "Ulm Forecast", "timestamp", "temp")
+    ]
     df_temp_plt_params = {"main": main_plot_params(df, "Temperature Over Time"), "inner": df_temp_inner_plt_params}
-    df_temp_24_plt_params = {'main': main_plot_params(last_24h_df(df),"Temperature Last 24 Hours", marker='o', markersize=6), "inner": df_temp_24_inner_plt_params}
+    df_temp_24_plt_params = {
+        'main': main_plot_params(last_24h_df(df), "Temperature Last 24 Hours", marker='o', markersize=6),
+        "inner": df_temp_24_inner_plt_params}
 
-    df_hum_plt_params = {"main": main_plot_params(df, "Humidity Over Time", y="humidity",  color="purple"), "inner": [inner_plots_params(google_df, "Google Forecast", "timestamp", "humidity")]}
-    df_hum_24_plt_params = {'main': main_plot_params(last_24h_df(df),"Humidity Last 24 Hours", marker='o', markersize=6, color="purple",ylabel="Humidity (%)", y="humidity"), "inner": [inner_24_plots_params(google_df, "Google Forecast", "timestamp", "humidity", alpha=None)]}
-    
-    plots_w_params = [df_temp_plt_params, df_temp_24_plt_params,df_hum_plt_params, df_hum_24_plt_params]
+    df_hum_plt_params = {"main": main_plot_params(df, "Humidity Over Time", y="humidity", color="purple"),
+                         "inner": [inner_plots_params(google_df, "Google Forecast", "timestamp", "humidity")]}
+    df_hum_24_plt_params = {
+        'main': main_plot_params(last_24h_df(df), "Humidity Last 24 Hours", marker='o', markersize=6, color="purple",
+                                 ylabel="Humidity (%)", y="humidity"),
+        "inner": [inner_24_plots_params(google_df, "Google Forecast", "timestamp", "humidity", alpha=None)]}
+
+    plots_w_params = [df_temp_plt_params, df_temp_24_plt_params, df_hum_plt_params, df_hum_24_plt_params]
     combined_fig, _ = create_multiple_lineplots(plots_w_params, theme=custom_theme, rows=2, cols=2)
     return combined_fig
 
 
-
-
-
-
-
-
-
-def main_plot_params(fr,title, label= "Home",xlabel="Time", x= "timestamp", ylabel="Temp (°C)",y="room_temp", marker=None, color=None, alpha=None, markersize=None):
+def main_plot_params(fr, title, label="Home", xlabel="Time", x="timestamp", ylabel="Temp (°C)", y="room_temp",
+                     marker=None, color=None, alpha=None, markersize=None):
     out = {
         "title": title,
         # if set overwrite x/y label with this
         "xlabel": xlabel,
-        "ylabel":ylabel,
+        "ylabel": ylabel,
     }
-    return {**out, **seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color,alpha=alpha,markersize=markersize)}
+    return {**out,
+            **seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color, alpha=alpha, markersize=markersize)}
 
 
+def inner_24_plots_params(fr, label, x, y, marker="o", markersize=6, alpha=0.6, color=None):
+    return seaborn_lineplot_params(last_24h_df(fr), label, x, y, marker=marker, color=color, alpha=alpha,
+                                   markersize=markersize)
 
-def inner_24_plots_params(fr, label, x, y, marker="o",markersize=6,alpha=0.6, color=None):
-    return seaborn_lineplot_params(last_24h_df(fr), label, x, y, marker=marker, color=color,alpha=alpha,markersize=markersize)
 
 def inner_plots_params(fr, label, x, y, marker=None, color=None, markersize=None):
-    return seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color,alpha=0.6,markersize=markersize)
+    return seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color, alpha=0.6, markersize=markersize)
 
 
-def seaborn_lineplot_params(fr, label, x, y,marker=None, color=None, alpha=None, markersize=None) -> dict:
+def seaborn_lineplot_params(fr, label, x, y, marker=None, color=None, alpha=None, markersize=None) -> dict:
     out = {
         "data": fr,
         "label": label,
         "x": x,
-        "y" : y,
+        "y": y,
     }
 
-     # optional seaborn parameters
+    # optional seaborn parameters
     if color:
         out["color"] = color
     if marker:
@@ -95,11 +97,11 @@ def seaborn_lineplot_params(fr, label, x, y,marker=None, color=None, alpha=None,
 
 
 # TODO: rename as it can create any combination of lineplots
-def create_multiple_lineplots(plot_params: List[dict], 
-                              fig_size: Tuple[int, int] = (25, 12), 
-                              rows:int = 1,
-                              cols:int = 1,
-                              theme: Optional[dict] = None, 
+def create_multiple_lineplots(plot_params: List[dict],
+                              fig_size: Tuple[int, int] = (25, 12),
+                              rows: int = 1,
+                              cols: int = 1,
+                              theme: Optional[dict] = None,
                               save_path: str = None,
                               despine=False) -> Tuple[plt.Figure, List[plt.Axes]]:
     """
@@ -116,17 +118,17 @@ def create_multiple_lineplots(plot_params: List[dict],
     if num_plots == 0:
         print("Noting to draw, nothing to return")
         return None, None
-    if rows <= 0 or cols <=0:
+    if rows <= 0 or cols <= 0:
         print(f"Rows and columns must be greater than zero but are {rows}, {cols}")
         return None, None
 
     if theme:
         sns.set_theme(**theme)
 
-    is_one_dimensional = (rows == 1 and cols >= 1) or (rows >=1 and cols == 1)
+    is_one_dimensional = (rows == 1 and cols >= 1) or (rows >= 1 and cols == 1)
     print(f"Creating {'1-dim' if is_one_dimensional else 'mult-dim'} {rows}x{cols} lineplots")
-    fig, axes = plt.subplots(rows,cols, figsize=fig_size)
-    
+    fig, axes = plt.subplots(rows, cols, figsize=fig_size)
+
     if num_plots == 1:
         # Ensure axes is a list even if there's only one plot
         axes = [axes]
@@ -135,7 +137,6 @@ def create_multiple_lineplots(plot_params: List[dict],
         # in multi dim its a List[...List[Axes]]
         axes = axes.T.flatten()
 
-    
     for idx, wrapper_dict in enumerate(plot_params):
         plot_dict = wrapper_dict["main"]
         data = plot_dict.pop('data')
@@ -144,7 +145,7 @@ def create_multiple_lineplots(plot_params: List[dict],
         ylabel = plot_dict.pop('ylabel', plot_dict.get('y'))
 
         print(f"plot {idx}: {plot_dict}")
-        
+
         ax_in_subplot = axes[idx]
         ax = sns.lineplot(data=data, ax=ax_in_subplot, **plot_dict)
         inner_plots = wrapper_dict.get('inner', None)
@@ -152,8 +153,8 @@ def create_multiple_lineplots(plot_params: List[dict],
             for inner_plot_dicts in inner_plots:
                 inner_data = inner_plot_dicts.pop("data")
                 print(f"inner plot {idx}: {inner_plot_dicts}")
-                sns.lineplot(data=inner_data, ax=ax,**inner_plot_dicts)
-            
+                sns.lineplot(data=inner_data, ax=ax, **inner_plot_dicts)
+
         if despine:
             sns.despine(left=True, bottom=True)
 
@@ -163,7 +164,6 @@ def create_multiple_lineplots(plot_params: List[dict],
 
         ax_in_subplot.set_title(title)
         ax_in_subplot.legend()
-  
 
     plt.tight_layout()
 
@@ -174,7 +174,6 @@ def create_multiple_lineplots(plot_params: List[dict],
 
     plt.close(fig)
     return fig, axes
-
 
 
 def _validate_and_sort_timestamp(data: pd.DataFrame) -> pd.DataFrame:
@@ -188,16 +187,18 @@ def _validate_and_sort_timestamp(data: pd.DataFrame) -> pd.DataFrame:
     """
     if 'timestamp' not in data.columns:
         raise ValueError("DataFrame must contain a 'timestamp' column.")
-    
+
     # Parse the 'timestamp' column and sort by it
-    data['timestamp'] = data['timestamp'].map(lambda x: datetime.strptime(str(x).replace("+00:00", "").strip(), '%Y-%m-%d %H:%M:%S'))
+    data['timestamp'] = data['timestamp'].map(
+        lambda x: datetime.strptime(str(x).replace("+00:00", "").strip(), '%Y-%m-%d %H:%M:%S'))
     data = data.sort_values(by="timestamp")
-    
+
     return data
 
+
 ##TODO: REMOVE
-def create_single_lineplot(data: pd.DataFrame, label: str, x_id: str, y_id: str, marker: str = "o", color: str = "blue", 
-                           fig_size: Tuple[int, int] = (10, 6), save_path: str = None, 
+def create_single_lineplot(data: pd.DataFrame, label: str, x_id: str, y_id: str, marker: str = "o", color: str = "blue",
+                           fig_size: Tuple[int, int] = (10, 6), save_path: str = None,
                            theme: Optional[dict] = None) -> Tuple[Figure, Axes]:
     """
     Creates a single line plot from the provided data using Seaborn and Matplotlib.
