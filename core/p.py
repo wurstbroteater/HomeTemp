e2087aaf9c0bcdf584cd3263113000b09e4f9014
wurstbroteater <fleri94@web.de>
2024-09-14 17:07:21 +0200

    update plot docu

--- core/p.py
+++ core/p.py
@@ -18,11 +18,9 @@ custom_theme = {
 }
 
 
-def last_24h_df(_df: pd.DataFrame) -> pd.DataFrame:
-    last_24h = datetime.now() - timedelta(hours=24)
-    return _df[_df['timestamp'] >= last_24h]
-
-
+# -
+# -------------------------------------------------- Main Methods --------------------------------------------------
+# -
 def draw_plots(df, dwd_df=None, google_df=None, wettercom_df=None, ulmde_df=None, with_save=True, save_path=None):
     df_temp_inner_plt_params = [
         inner_plots_params(dwd_df, "DWD Forecast", "timestamp", "temp"),
@@ -51,12 +49,21 @@ def draw_plots(df, dwd_df=None, google_df=None, wettercom_df=None, ulmde_df=None
         "inner": [inner_24_plots_params(google_df, "Google Forecast", "timestamp", "humidity", alpha=None)]}
 
     plots_w_params = [df_temp_plt_params, df_temp_24_plt_params, df_hum_plt_params, df_hum_24_plt_params]
-    combined_fig, _ = create_multiple_lineplots(plots_w_params, theme=custom_theme, rows=2, cols=2)
+    combined_fig, _ = create_lineplots(plots_w_params, theme=custom_theme, rows=2, cols=2)
     return combined_fig
 
 
-def main_plot_params(fr, title, label="Home", xlabel="Time", x="timestamp", ylabel="Temp (°C)", y="room_temp",
-                     marker=None, color=None, alpha=None, markersize=None):
+# -
+# -------------------------------------------------- Util Methods --------------------------------------------------
+# -
+def last_24h_df(_df: pd.DataFrame) -> pd.DataFrame:
+    last_24h = datetime.now() - timedelta(hours=24)
+    return _df[_df['timestamp'] >= last_24h]
+
+
+def main_plot_params(fr, title,
+                     label="Home", xlabel="Time", x="timestamp", ylabel="Temp (°C)", y="room_temp",
+                     marker=None, color=None, alpha=None, markersize=None) -> dict:
     out = {
         "title": title,
         # if set overwrite x/y label with this
@@ -67,12 +74,12 @@ def main_plot_params(fr, title, label="Home", xlabel="Time", x="timestamp", ylab
             **seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color, alpha=alpha, markersize=markersize)}
 
 
-def inner_24_plots_params(fr, label, x, y, marker="o", markersize=6, alpha=0.6, color=None):
+def inner_24_plots_params(fr, label, x, y, marker="o", markersize=6, alpha=0.6, color=None) -> dict:
     return seaborn_lineplot_params(last_24h_df(fr), label, x, y, marker=marker, color=color, alpha=alpha,
                                    markersize=markersize)
 
 
-def inner_plots_params(fr, label, x, y, marker=None, color=None, markersize=None):
+def inner_plots_params(fr, label, x, y, marker=None, color=None, markersize=None) -> dict:
     return seaborn_lineplot_params(fr, label, x, y, marker=marker, color=color, alpha=0.6, markersize=markersize)
 
 
@@ -96,22 +103,48 @@ def seaborn_lineplot_params(fr, label, x, y, marker=None, color=None, alpha=None
     return out
 
 
-# TODO: rename as it can create any combination of lineplots
-def create_multiple_lineplots(plot_params: List[dict],
-                              fig_size: Tuple[int, int] = (25, 12),
-                              rows: int = 1,
-                              cols: int = 1,
-                              theme: Optional[dict] = None,
-                              save_path: str = None,
-                              despine=False) -> Tuple[plt.Figure, List[plt.Axes]]:
+def _validate_and_sort_timestamp(data: pd.DataFrame) -> pd.DataFrame:
     """
-    Create multiple subplots of line plots using a list of dictionaries for plot parameters.
+    Private method to validate that the DataFrame contains a 'timestamp' column.
+    Parses the 'timestamp' column and sorts the DataFrame by 'timestamp'.
 
-    :param plot_params: A list of dictionaries, where each dictionary contains:
-                        {"data": pd.DataFrame, **sns.lineplot keyword arguments}
+    :param data: The Pandas DataFrame containing the data.
+    :return: The validated and sorted DataFrame.
+    :raises ValueError: If 'timestamp' column is missing.
+    """
+    if 'timestamp' not in data.columns:
+        raise ValueError("DataFrame must contain a 'timestamp' column.")
+
+    # Parse the 'timestamp' column and sort by it
+    data['timestamp'] = data['timestamp'].map(
+        lambda x: datetime.strptime(str(x).replace("+00:00", "").strip(), '%Y-%m-%d %H:%M:%S'))
+    data = data.sort_values(by="timestamp")
+
+    return data
+
+
+# -
+# -------------------------------------------------- Draw Methods --------------------------------------------------
+# -
+def create_lineplots(plot_params: List[dict],
+                     fig_size: Tuple[int, int] = (25, 12),
+                     rows: int = 1,
+                     cols: int = 1,
+                     theme: Optional[dict] = None,
+                     save_path: str = None,
+                     despine=False) -> Tuple[plt.Figure, List[plt.Axes]]:
+    """
+    Create multiple or one dimensional subplots of line plots using a list of dictionaries for plot parameters.
+
+    :param plot_params: A list of dictionaries, where each dictionary contains the key "main" which is a dict of
+                        parameters for the main plot. It may contain the key "inner" which is a list of dict of
+                        parameters for echo inner plot.
     :param fig_size: The overall size of the figure (default is (15, 10)).
+    :param rows: The number of rows in the figure (default is 1).
+    :param cols: The number of columns in the figure (default is 1).
     :param theme: Optional dictionary to customize Seaborn theme (default is None).
     :param save_path: Optional path to save the figure as a file (default is None).
+    :param despine: Optional flag to despine the subplots (default is False).
     :return: A tuple containing the Figure and list of Axes objects.
     """
     num_plots = len(plot_params)
@@ -174,60 +207,3 @@ def create_multiple_lineplots(plot_params: List[dict],
 
     plt.close(fig)
     return fig, axes
-
-
-def _validate_and_sort_timestamp(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Private method to validate that the DataFrame contains a 'timestamp' column.
-    Parses the 'timestamp' column and sorts the DataFrame by 'timestamp'.
-
-    :param data: The Pandas DataFrame containing the data.
-    :return: The validated and sorted DataFrame.
-    :raises ValueError: If 'timestamp' column is missing.
-    """
-    if 'timestamp' not in data.columns:
-        raise ValueError("DataFrame must contain a 'timestamp' column.")
-
-    # Parse the 'timestamp' column and sort by it
-    data['timestamp'] = data['timestamp'].map(
-        lambda x: datetime.strptime(str(x).replace("+00:00", "").strip(), '%Y-%m-%d %H:%M:%S'))
-    data = data.sort_values(by="timestamp")
-
-    return data
-
-
-##TODO: REMOVE
-def create_single_lineplot(data: pd.DataFrame, label: str, x_id: str, y_id: str, marker: str = "o", color: str = "blue",
-                           fig_size: Tuple[int, int] = (10, 6), save_path: str = None,
-                           theme: Optional[dict] = None) -> Tuple[Figure, Axes]:
-    """
-    Creates a single line plot from the provided data using Seaborn and Matplotlib.
-
-    :param data: The Pandas DataFrame containing the data. Must contain a 'timestamp' column.
-    :param label: The label for the plot.
-    :param x_id: The column name for the x-axis data.
-    :param y_id: The column name for the y-axis data.
-    :param marker: The marker style for the plot (default is 'o').
-    :param color: The color of the line (default is 'blue').
-    :param fig_size: The size of the figure (default is (10, 6)).
-    :param save_path: Optional path to save the figure as a file (default is None).
-    :param theme: Optional dictionary to customize Seaborn theme (default is None).
-    :return: A tuple containing the Figure and Axes objects.
-    """
-    data = _validate_and_sort_timestamp(data)
-
-    if theme:
-        sns.set_theme(**theme)
-
-    fig, ax = plt.subplots(figsize=fig_size)
-    sns.lineplot(label=label, x=x_id, y=y_id, color=color, marker=marker, data=data, ax=ax)
-    ax.set_xlabel(x_id)
-    ax.set_ylabel(y_id)
-
-    if save_path is not None:
-        save_file = os.path.join(save_path, 'TODO_FILENAME.pdf')
-        fig.savefig(save_file)
-        log.info(f'Plot saved to {save_file}')
-
-    plt.close(fig)
-    return fig, ax
