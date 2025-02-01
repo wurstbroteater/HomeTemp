176ad741b05da2a2af8a9e77e16f4d0a4b89ad85
wurstbroteater <fleri94@web.de>
2025-02-01 17:52:38 +0100

    add start script

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+start_screen() {
+    SCREEN_NAME=$1
+    COMMAND=$2
+    screen -dmS "$SCREEN_NAME" bash -c "source .venv/bin/activate && $COMMAND"
+    echo "Started screen '$SCREEN_NAME' running: $COMMAND"
+}
+
+# Check if correct argument was provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 {hometemp|basetemp}"
+    exit 1
+fi
+
+# Requires screen command to be installed!
+# starts detached screens for hometemp or basetemp
+case "$1" in
+    hometemp)
+        start_screen "temps" "python hometemp.py"
+        start_screen "dwd" "python fetch_forecasts.py"
+        ;;
+    basetemp)
+        start_screen "base" "python basetemp.py"
+        ;;
+    *)
+        echo "Invalid option: $1"
+        echo "Usage: $0 {hometemp|basetemp}"
+        exit 1
+        ;;
+esac
